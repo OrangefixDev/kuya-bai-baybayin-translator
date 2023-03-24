76978852a8eb9869c6ebe3928859bdff24828481
Esquire111 <126742498+Esquire111@users.noreply.github.com>
2023-03-24 11:47:04 +0800

    updated about us
    using popper js

--- js/index.js
+++ js/index.js
@@ -158,6 +158,29 @@ document.addEventListener('DOMContentLoaded', () => {
   const btn = document.getElementById('share-btn');
   const shareOptions = document.querySelector('.share-options');
 
+  function show() {
+    tooltip.setAttribute('data-show', '');
+  
+    // We need to tell Popper to update the tooltip position
+    // after we show the tooltip, otherwise it will be incorrect
+    popperInstance.update();
+  }
+  
+  function hide() {
+    tooltip.removeAttribute('data-show');
+  }
+  
+  const showEvents = ['mouseenter', 'focus'];
+  const hideEvents = ['mouseleave', 'blur'];
+  
+  showEvents.forEach((event) => {
+    button.addEventListener(event, show);
+  });
+  
+  hideEvents.forEach((event) => {
+    button.addEventListener(event, hide);
+  });
+
   //const btn = document.getElementById('share-btn');
   //const shareOptions = document.querySelector('.share-options');
   console.log(btn);
